6a99347423707f45bdcdce86e7fa4dd4d3db2926
thukhaaung <thukhaaung@gmail.com>
2020-04-21 11:31:40 +0630

    First Commit

---
+++ mytest-demo/src/test/java/com/mytest/springboot/mytestdemo/MytestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytest.springboot.mytestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
